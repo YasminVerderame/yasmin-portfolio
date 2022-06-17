ab8d8152ffc32a65767d97af0e97f8e149e52dda
Yasmin Verderame Braz <yasmin.verderame@lmcorp.local>
2022-06-17 16:57:45 -0300

    organizando components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,17 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
-
+import { HomeComponent } from './pages/home/home.component';
 
 @NgModule({
   declarations: [
     AppComponent,
+    HomeComponent,
+
     
   ],
   imports: [
